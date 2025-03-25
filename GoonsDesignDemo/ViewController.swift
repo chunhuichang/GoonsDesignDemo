a4b1bfd0409047f5dff16a8812c49c86691e3885
Jill Chang <jill20021@gmail.com>
2025-03-25 11:24:10 +0800

    feat: Configure app entry point programmatically

--- GoonsDesignDemo/ViewController.swift
+++ GoonsDesignDemo/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
